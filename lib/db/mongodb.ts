57ba2b9f9a16d056ac644d45e3fd3abb8f2c2d6e
Roy Adams <roy@eden-grafix.co.uk>
2025-06-03 12:05:24 +0100

    More debuging

--- lib/db/mongodb.ts
+++ lib/db/mongodb.ts
@@ -3,15 +3,19 @@ import { ENV } from "../services/envService";
 
 let cachedDb: Db | null = null;
 
-export async function connectToDB(): Promise<Db> {
+export async function connectToDB(): Promise<Db | undefined> {
   if (cachedDb) return cachedDb;
   console.log("ENV.MONGODB_DB_NAME: ", ENV.MONGODB_DB_NAME);
   console.log("cachedDb: ", cachedDb);
-  const client = new MongoClient(ENV.MONGODB_URI);
-  console.log("ENV.MONGODB_URI: ", ENV.MONGODB_URI);
-  console.log("client: ", client);
-  await client.connect();
-  cachedDb = client.db(ENV.MONGODB_DB_NAME);
-  console.log("client: cachedDb", cachedDb);
-  return cachedDb;
+  try {
+    const client = new MongoClient(ENV.MONGODB_URI);
+    console.log("ENV.MONGODB_URI: ", ENV.MONGODB_URI);
+    console.log("client: ", client);
+    await client.connect();
+    cachedDb = client.db(ENV.MONGODB_DB_NAME);
+    console.log("client: cachedDb", cachedDb);
+    return cachedDb;
+  } catch (error) {
+    console.error("Failed to connect to DB:", error);
+  }
 }
