027b24d1f63d028b2ea38dde4fe76eabe0ca4e15
Roy Adams <roy@eden-grafix.co.uk>
2025-06-03 11:37:06 +0100

    debuging

--- lib/db/mongodb.ts
+++ lib/db/mongodb.ts
@@ -5,9 +5,11 @@ let cachedDb: Db | null = null;
 
 export async function connectToDB(): Promise<Db> {
   if (cachedDb) return cachedDb;
-
+  console.log("ENV.MONGODB_DB_NAME: ", ENV.MONGODB_DB_NAME);
+  console.log("cachedDb: ", cachedDb);
   const client = new MongoClient(ENV.MONGODB_URI);
   await client.connect();
   cachedDb = client.db(ENV.MONGODB_DB_NAME);
+  console.log("client: cachedDb", cachedDb);
   return cachedDb;
 }
