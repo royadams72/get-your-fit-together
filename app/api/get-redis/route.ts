8370ee86ec563b48aa63febd747bfbe6dd3a46ba
Roy Adams <roy@eden-grafix.co.uk>
2025-06-19 19:23:06 +0100

    small change

--- app/api/get-redis/route.ts
+++ app/api/get-redis/route.ts
@@ -2,13 +2,25 @@ import { NextResponse } from "next/server";
 
 import redis from "@/lib/db/redisClient";
 
-import { Cookie } from "@/types/enums/cookie.enum";
-import { cookies } from "next/headers";
+import { Cookie, CookieAction } from "@/types/enums/cookie.enum";
 
 export async function GET() {
   try {
-    const cookieStore = await cookies();
-    const sessionCookie = cookieStore.get("sessionCookie")?.value;
+    let sessionCookie = await cookieAction(CookieAction.get, [
+      Cookie.sessionCookie,
+    ]);
+    let retries = 3;
+    while (!sessionCookie && retries > 0) {
+      console.log("GET retries:", retries, sessionCookie);
+
+      if (!sessionCookie) {
+        await new Promise((res) => setTimeout(res, 100));
+        sessionCookie = await cookieAction(CookieAction.get, [
+          Cookie.sessionCookie,
+        ]);
+        retries--;
+      }
+    }
 
     const redisCache = await redis.get(
       `${Cookie.sessionCookie}:${sessionCookie}`
@@ -27,4 +39,10 @@ export async function GET() {
       { status: 500 }
     );
   }
+}
+function cookieAction(
+  get: any,
+  arg1: Cookie[]
+): string | PromiseLike<string | undefined> | undefined {
+  throw new Error("Function not implemented.");
 }
