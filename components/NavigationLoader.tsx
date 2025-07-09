58a115fbaed8115b990e04b4c5fb6761091292e0
Roy Adams <roy@eden-grafix.co.uk>
2025-07-09 15:15:22 +0100

    testing SW

--- components/NavigationLoader.tsx
+++ components/NavigationLoader.tsx
@@ -4,6 +4,7 @@ import Loader from "@/context/Loader/Loader";
 
 const NavigationLoader = () => {
   const isLoading = useOnNavigate();
+  console.log("NavigationLoader loading::", isLoading);
 
   if (!isLoading) return null;
 
